42ab2cc4444d191e1ba6cc6ad10b54ddd318e9e2
“linjunjj” <1045735402@qq.com>
2018-11-05 11:20:32 +0800

    Initial commit

---
+++ commonUtils.go
@@ -0,0 +1,154 @@
+package encryptTool
+
+import (
+	"crypto/md5"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"net/url"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func StringToMap(data string) (dataS map[string]string) {
+	datas := make(map[string]string)
+	splitData := strings.Split(data, "&")
+	for _, v := range splitData {
+		vd := strings.Split(v, "=")
+		datas[vd[0]] = vd[1]
+	}
+	return datas
+}
+
+//获取当前年月日时分秒
+func getTrace() string {
+	return time.Now().Format("20060102150405")
+}
+
+//获取当年年月日
+func getTimeFromymd() string {
+	return time.Now().Format("20060102")
+}
+
+//获取当前时分秒
+
+func getTimeFromSec() string {
+	return time.Now().Format("150405")
+}
+
+/**
+  生成随机字符串
+*/
+func randomString(lens int) string {
+	now := time.Now()
+	return makeMd5(strconv.FormatInt(now.UnixNano(), 10))[:lens]
+}
+
+/**
+  字符串md5
+*/
+func makeMd5(str string) string {
+	h := md5.New()
+	io.WriteString(h, str)
+	s := fmt.Sprintf("%x", h.Sum(nil))
+	return s
+}
+
+/**
+  获取当前时间戳
+*/
+func getNowSec() int64 {
+	return time.Now().Unix()
+}
+
+/**
+  获取当前时间戳
+*/
+func str2Sec(layout, str string) int64 {
+	tm2, _ := time.ParseInLocation(layout, str, time.Local)
+	return tm2.Unix()
+}
+
+/**
+  获取当前时间
+*/
+func sec2Str(layout string, sec int64) string {
+	t := time.Unix(sec, 0)
+	nt := t.Format(layout)
+	return nt
+}
+
+// base64 加密
+func base64Encode(data []byte) string {
+	return base64.StdEncoding.EncodeToString(data)
+}
+
+// base64 解密
+func base64Decode(data string) ([]byte, error) {
+	return base64.StdEncoding.DecodeString(data)
+}
+
+type mapSorter []sortItem
+
+type sortItem struct {
+	Key string      `json:"key"`
+	Val interface{} `json:"val"`
+}
+
+func (ms mapSorter) Len() int {
+	return len(ms)
+}
+func (ms mapSorter) Less(i, j int) bool {
+	return ms[i].Key < ms[j].Key // 按键排序
+}
+func (ms mapSorter) Swap(i, j int) {
+	ms[i], ms[j] = ms[j], ms[i]
+}
+func mapSortByKey(m map[string]string, step1, step2 string) string {
+	ms := make(mapSorter, 0, len(m))
+
+	for k, v := range m {
+		ms = append(ms, sortItem{k, v})
+	}
+	sort.Sort(ms)
+	s := []string{}
+	for _, p := range ms {
+		s = append(s, p.Key+step1+p.Val.(string))
+	}
+	return strings.Join(s, step2)
+}
+func timeoutClient() *http.Client {
+	connectTimeout := time.Duration(20 * time.Second)
+	readWriteTimeout := time.Duration(30 * time.Second)
+	return &http.Client{
+		Transport: &http.Transport{
+			Dial:                timeoutDialer(connectTimeout, readWriteTimeout),
+			MaxIdleConnsPerHost: 200,
+			DisableKeepAlives:   true,
+		},
+	}
+}
+func timeoutDialer(cTimeout time.Duration,
+	rwTimeout time.Duration) func(net, addr string) (c net.Conn, err error) {
+	return func(netw, addr string) (net.Conn, error) {
+		conn, err := net.DialTimeout(netw, addr, cTimeout)
+		if err != nil {
+			return nil, err
+		}
+		conn.SetDeadline(time.Now().Add(rwTimeout))
+		return conn, nil
+	}
+}
+
+// urlencode
+func Http_build_query(params map[string]string) string {
+	qs := url.Values{}
+	for k, v := range params {
+		qs.Add(k, v)
+	}
+	return qs.Encode()
+}
---
+++ rsaUtil.go
@@ -0,0 +1,257 @@
+package encryptTool
+
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"golang.org/x/crypto/pkcs12"
+	"io/ioutil"
+	"net/url"
+)
+
+//初始使用的配置
+type Config struct {
+	// pfx 证书路径,和同时传入PrivatePath和CertPath 效果一样
+	PfxPath string
+	// pfx 证书的密码
+	PfxPwd string
+	//证书是否需要base64解密
+	IsDecode bool
+	// 验签私钥证书地址，传入pfx此路径可不传
+	PrivatePath string
+	// 公钥证书地址,传入pfx此路径可以不传
+	PublicPath string
+	// pfx加密证书地址
+	EncryptCertPath string
+}
+
+// 证书信息结构体
+type Cert struct {
+	// 私钥 签名使用
+	Private *rsa.PrivateKey
+	// 证书 与私钥为一套
+	Cert *x509.Certificate
+	// 签名证书ID
+	CertId string
+	// 加密证书
+	EncryptCert *x509.Certificate
+	// 公钥 加密验签使用
+	Public *rsa.PublicKey
+	// 加密公钥ID
+	EncryptId string
+}
+
+
+
+func NewInstance(config *Config) *Cert {
+	if config.PfxPwd != "" && config.PfxPath != "" {
+		private, certdata, _ := parserPfxToCert(config.PfxPath, config.PfxPwd, config.IsDecode)
+		encryptCert, _ := parseCertificateFromFile(config.EncryptCertPath)
+		//certData.Public = certData.EncryptCert.PublicKey.(*rsa.PublicKey)
+
+		return &Cert{
+			Private:     private,
+			Cert:        certdata,
+			EncryptCert: encryptCert,
+		}
+	} else if config.PublicPath != "" && config.PrivatePath != "" {
+		private, _ := parsePrivateFromFile(config.PrivatePath)
+
+		public, _ := parsePublicFromFile(config.PublicPath)
+		return &Cert{
+			Private: private,
+			Public:  public,
+		}
+	} else {
+		panic("请输入有效初始数据")
+	}
+
+}
+
+
+
+// 根据PFX文件和密码来解析出里面包含的私钥(rsa)和证书(x509)
+func parserPfxToCert(path string, password string, isbase64decode bool) (private *rsa.PrivateKey, cert *x509.Certificate, err error) {
+	var pfxData []byte
+	pfxData, err = ioutil.ReadFile(path)
+	if isbase64decode {
+		fmt.Println(string(pfxData))
+		pfxData, _ = base64Decode(string(pfxData))
+
+	}
+	if err != nil {
+		return
+	}
+	var priv interface{}
+	priv, cert, err = pkcs12.Decode(pfxData, password)
+	if err != nil {
+		return
+	}
+	private = priv.(*rsa.PrivateKey)
+	return
+}
+
+// 根据文件名解析出私钥 ,文件必须是rsa 私钥格式。
+func parsePrivateFromFile(path string) (private *rsa.PrivateKey, err error) {
+	// Read the private key
+	pemData, err := ioutil.ReadFile(path)
+	if err != nil {
+		err = fmt.Errorf("read key file: %s", err)
+		return
+	}
+
+	// Extract the PEM-encoded data block
+	block, _ := pem.Decode(pemData)
+	if block == nil {
+		err = fmt.Errorf("bad key data: %s", "not PEM-encoded")
+		return
+	}
+
+	if block.Type == "RSA PRIVATE KEY" {
+
+		if got, want := block.Type, "RSA PRIVATE KEY"; got != want {
+			err = fmt.Errorf("unknown key type %q, want %q", got, want)
+			return
+		}
+		private, err = x509.ParsePKCS1PrivateKey(block.Bytes)
+		if err != nil {
+			err = fmt.Errorf("bad private key: %s", err)
+			return
+		}
+		return
+
+	} else if block.Type == "PRIVATE KEY" {
+		if got, want := block.Type, "PRIVATE KEY"; got != want {
+			err = fmt.Errorf("unknown key type %q, want %q", got, want)
+			return
+		}
+		privates, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+		private = privates.(*rsa.PrivateKey)
+		if err != nil {
+			err = fmt.Errorf("bad private key: %s", err)
+		}
+	}
+	return
+}
+
+// 根据文件名解析出私钥 ,文件必须是rsa 公钥格式。
+func parsePublicFromFile(path string) (private *rsa.PublicKey, err error) {
+	// Read the private key
+	pemData, err := ioutil.ReadFile(path)
+	if err != nil {
+		err = fmt.Errorf("read key file: %s", err)
+		return
+	}
+
+	// Extract the PEM-encoded data block
+	block, _ := pem.Decode(pemData)
+	if block == nil {
+		err = fmt.Errorf("bad key data: %s", "not PEM-encoded")
+		return
+	}
+
+	if block.Type == "RSA PRIVATE KEY" {
+
+		if got, want := block.Type, "RSA PRIVATE KEY"; got != want {
+			err = fmt.Errorf("unknown key type %q, want %q", got, want)
+			return
+		}
+		private, err = x509.ParsePKCS1PublicKey(block.Bytes)
+		if err != nil {
+			err = fmt.Errorf("bad private key: %s", err)
+			return
+		}
+		return
+
+	}
+	return
+}
+
+// 根据文件名解析出证书
+func parseCertificateFromFile(path string) (cert *x509.Certificate, err error) {
+	// Read the verify sign certification key
+	pemData, err := ioutil.ReadFile(path)
+	fmt.Println(pemData)
+	if err != nil {
+		return
+	}
+
+	// Extract the PEM-encoded data block
+	block, _ := pem.Decode(pemData)
+	if block == nil {
+		err = fmt.Errorf("bad key data: %s", "not PEM-encoded")
+		return
+	}
+	if got, want := block.Type, "CERTIFICATE"; got != want {
+		err = fmt.Errorf("unknown key type %q, want %q", got, want)
+		return
+	}
+
+	// Decode the certification
+	cert, err = x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		err = fmt.Errorf("bad private key: %s", err)
+		return
+	}
+	return
+}
+
+// 利用加密证书公钥对数据加密
+func (c *Cert) EncryptData(data string) (res string, err error) {
+	if c.EncryptId == "" {
+		err = fmt.Errorf("请先配置加密证书信息")
+		return
+	}
+	rng := rand.Reader
+	signer, err := rsa.EncryptPKCS1v15(rng, c.Public, []byte(data))
+	res = base64Encode(signer)
+	return
+}
+
+// sign 做签
+func (c *Cert) SignDataBySha256(request map[string]string) (string, error) {
+	str := mapSortByKey(request, "=", "&")
+	rng := rand.Reader
+	hashed := sha256.Sum256([]byte(fmt.Sprintf("%x", sha256.Sum256([]byte(str)))))
+	signer, err := rsa.SignPKCS1v15(rng, c.Private, crypto.SHA256, hashed[:])
+	if err != nil {
+		return "", err
+	}
+	return base64Encode(signer), nil
+}
+
+
+// 返回数据验签Sha256
+func (c *Cert) VerifyDataBySha256(vals url.Values) (res interface{}, err error) {
+	var signature string
+	kvs := map[string]string{}
+	for k := range vals {
+		if k == "signature" {
+			signature = vals.Get(k)
+			continue
+		}
+		if vals.Get(k) == "" {
+			continue
+		}
+		kvs[k] = vals.Get(k)
+	}
+	str := mapSortByKey(kvs, "=", "&")
+	hashed := sha256.Sum256([]byte(fmt.Sprintf("%x", sha256.Sum256([]byte(str)))))
+	var inSign []byte
+	inSign, err = base64Decode(signature)
+	if err != nil {
+		return nil, fmt.Errorf("解析返回signature失败 %v", err)
+	}
+
+	err = rsa.VerifyPKCS1v15(c.Public, crypto.SHA256, hashed[:], inSign)
+	if err != nil {
+		return nil, fmt.Errorf("返回数据验签失败 ERR:%v", err)
+	}
+	return kvs, nil
+}
+
+
